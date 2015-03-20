fd3b4d8e2d4ab34b98471904413180ab9f210d23
Ben Dooks <ben-linux@fluff.org>
2015-03-20 22:02:28 +0000

    add initial i2c code

---
+++ i2c-code.h
@@ -0,0 +1,177 @@
+
+static inline void i2c_delay(void)
+{
+	// this gives us a bus speed of around 1kHz
+	// too much faster and the pic tends to start nacking data
+	// 10 gives us around 40kHz - needs PIC running around 16MHZ
+	// 1 give us around the 110kHz rate
+	_delay_loop_2(1);
+}
+
+static void i2c_start(void)
+{
+	sda_high();
+	i2c_delay();
+	scl_high();
+	i2c_delay();
+	sda_low();
+	i2c_delay();
+	scl_low();
+	i2c_delay();
+}
+
+static void i2c_stop(void)
+{
+	sda_low();
+	i2c_delay();
+	scl_high();
+	i2c_delay();
+	sda_high();
+	i2c_delay();	
+}
+
+static void wait_scl(void)
+{
+	unsigned int timeout = 0x2000;
+
+	for (; timeout > 0; timeout--) {
+		if (i2c_get_scl() != 0)
+			return;
+	}
+
+	printf_P(PSTR("timeout waiting for scl, SCL=%d, SDA=%d\n"),
+		 i2c_get_scl(), i2c_get_sda());
+}
+
+static int i2c_tx(unsigned char byte)
+{
+	unsigned bit;
+
+	//printf_P(PSTR("tx(%x)\n"), byte);
+
+	for (bit = 8; bit > 0; bit--) {
+		//printf_P(PSTR("send bit (%d)\n"), bit);
+		if (byte & (0x80))
+			sda_high();
+		else
+			sda_low();
+
+		i2c_delay();
+
+		scl_high();
+		wait_scl();
+
+		byte <<= 1;
+		i2c_delay();
+
+		scl_low();
+		i2c_delay();
+	}
+
+	//printf_P(PSTR("await ack\n"));
+	sda_high();
+	scl_high();
+	wait_scl();
+
+	i2c_delay();
+	
+	byte = i2c_get_sda();
+	scl_low();
+	i2c_delay();
+
+	//printf_P(PSTR("done byte"));
+	return byte ? 1 : 0;
+}
+
+static void i2c_write_bit(unsigned bit)
+{
+	if (bit)
+		sda_high();
+	else
+		sda_low();
+
+	i2c_delay();
+	scl_high();
+	wait_scl();
+
+	i2c_delay();
+	scl_low();
+	i2c_delay();
+}
+
+static unsigned char i2c_rx(unsigned ack)
+{
+	unsigned bit;
+	unsigned char result = 0;
+
+	/* ensure we are not driving sda before we start receiving */
+	sda_high();
+	i2c_delay();
+
+	for (bit = 8; bit > 0; bit--) {
+		//printf_P(PSTR("reading bit %d\n"), bit);
+		scl_high();
+		wait_scl();
+		i2c_delay();
+
+		result <<= 1;
+		result |= i2c_get_sda() ? 1 : 0;
+		
+		scl_low();
+		i2c_delay();
+	}
+
+	i2c_write_bit(ack);
+	i2c_delay();
+
+	return result;
+}
+
+int iic_send_bytes(unsigned char addr, unsigned size,
+		   const unsigned char *data)
+{
+	unsigned char ret;
+
+	i2c_start();
+	ret = i2c_tx(addr << 1);
+	if (ret) {
+		//printf_P(PSTR("tx: nack addr\n"));
+		goto err;
+	}
+
+	for (; size > 0; size--, data++) {
+		ret = i2c_tx(*data);
+		if (ret) {
+			printf_P(PSTR("tx: nacked byte\n"));
+			goto err;
+		}
+	}
+
+	i2c_stop();
+	return 0;
+
+err:
+	i2c_stop();
+	return 1;
+}
+
+int iic_receive_byte(unsigned char addr)
+{
+	unsigned char result;
+	unsigned char ret;
+
+	i2c_start();
+
+	//printf_P(PSTR("sending addr %x\n"), ((addr << 1) | 1));
+	ret = i2c_tx((addr << 1) | 1);
+	if (ret) {
+		i2c_stop();
+		return -1;
+	}
+
+	//printf_P(PSTR("reading result\n"));
+	result = i2c_rx(0);
+
+	i2c_stop();
+	return result;
+}
---
+++ i2c.cpp
@@ -0,0 +1,42 @@
+// PD4 = SDA
+// PD5 = SCL
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include <LUFA/Drivers/USB/USB.h>
+#include <LUFA/Platform/Platform.h>
+
+#include <util/delay.h>
+
+// Workaround for http://gcc.gnu.org/bugzilla/show_bug.cgi?id=34734
+#undef PROGMEM
+#define PROGMEM __attribute__((section(".progmem.data")))
+
+
+#define SDA	(1 << 6)
+#define SCL	(1 << 4)
+
+static inline void sda_high(void) { DDRC &= ~SDA; }
+static inline void sda_low(void)  { DDRC |= SDA; }
+
+// moved SCL to pin pc5 as debug
+
+static inline void scl_high(void) { DDRC &= ~SCL; }
+static inline void scl_low(void)  { DDRC |= SCL; }
+
+static inline unsigned char i2c_get_scl(void) { return PINC & SCL; }
+static inline unsigned char i2c_get_sda(void) { return PINC & SDA; }
+
+#define iic_send_bytes i2c_send_bytes
+#define iic_receive_byte i2c_receive_byte
+
+#include "i2c-code.h"
+
+void i2c_init(void)
+{
+	sda_high();     // default to input
+	scl_high();     // default to input
+	PORTD &= ~SDA;  // drive low when output
+	PORTD &= ~SCL;  // drive low when output
+}
